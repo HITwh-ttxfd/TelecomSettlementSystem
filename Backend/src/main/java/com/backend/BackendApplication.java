8c83108534a1187a49dc0c4732447ac35e0025d1
GODDOG-wyc <GODDOG-wyc@163.com>
2020-07-29 16:31:33 +0800

    配置数据库

--- Backend/src/main/java/com/backend/BackendApplication.java
+++ Backend/src/main/java/com/backend/BackendApplication.java
@@ -2,12 +2,13 @@ package com.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-//嘎嘎嘎
+
 }
