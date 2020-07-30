762e7fcab79f65689869b643f7563c0a9b6f01ce
GODDOG-wyc <GODDOG-wyc@163.com>
2020-07-30 22:00:33 +0800

    实体类

--- Backend/src/main/java/com/backend/controller/ChuZhangShouRuController.java
+++ Backend/src/main/java/com/backend/controller/ChuZhangShouRuController.java
@@ -4,6 +4,8 @@ import com.backend.entity.RpCityCodeT;
 import com.backend.mapper.RpCityCodeTMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,12 +13,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/list")
 public class ChuZhangShouRuController {
-//    @Autowired
-//    private RpCityCodeTMapper rpCityCodeTMapper;
-//
+    @Autowired
+    private RpCityCodeTMapper rpCityCodeTMapper;
+
 //    @RequestMapping("/test")
 //    public List<RpCityCodeT> list() {
-//
+//        System.out.println("测试成功1");
 //        List<RpCityCodeT> list = rpCityCodeTMapper.selectAll();
 //        return  list;
 //    }
@@ -25,6 +27,15 @@ public class ChuZhangShouRuController {
     public String HelloController() {
         System.out.println("测试成功");
         return "hello wx";
+    }
+
+    @RequestMapping(value = "/search",method = {RequestMethod.GET})
+    public List<RpCityCodeT> HelloController1(@RequestParam(required=false) String cityCode,@RequestParam(required=false) String cityName) {
+        RpCityCodeT rpCityCodeT = new RpCityCodeT();
+        rpCityCodeT.setCityCode(cityCode);
+        rpCityCodeT.setCityName(cityName);
+
+        return rpCityCodeTMapper.selectAll(rpCityCodeT);
     }
 
 
