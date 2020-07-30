bb96723778c82c6a9c38c209bcd877c7ca7361aa
Neo1fight1 <17861080078@163.com>
2020-07-30 16:41:50 +0800

    按照MyBatis的格式创建项目目录，以及一些没传上去的

---
+++ Backend/src/main/java/com/backend/CorsConfig.java
@@ -0,0 +1,22 @@
+
+package com.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *purpose:设置跨域请求
+ * date:2020-7-30
+ * 原网址：https://blog.csdn.net/tiangongkaiwu152368/article/details/81099169?depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromBaidu-1&utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromBaidu-1
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
+
